06ef103c580ad58d93de7465a07002b1df929a1f
Anuj Potnis <aspotnis@mtu.edu>
2017-01-31 16:57:00 -0500

    Renamed files so that they can be imported as modules

---
+++ term-1/sandbox/Untitled.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print(5+3)
+
+
+# In[ ]:
+
+'''
+http://stackoverflow.com/questions/19564625/how-can-i-import-from-another-ipython-notebook
+
+When you start ipython use the --script flag: For example
+
+ipython notebook --script
+Then whenever you save your notebook "common_func.ipnb" it will also create a file entitled "common_func.py." You can import functions from that by using
+
+from common_func import func_a
+If you change the common_func notebook, you may need to use
+
+reload()
+'''
+
+
+# In[ ]:
+
+'''
+name python modules without hyphen. Underscore is okay
+'''
+
